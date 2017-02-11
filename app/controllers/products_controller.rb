278973e2e56f08ac919affa25053163171a22254
Bill Klinkow <klinkow@gmail.com>
2017-02-10 16:07:28 -0800

    add materialize and full crud

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,42 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render :show
   end
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to  products_path
+    else
+      render :new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :coo)
+  end
 end
--- db/migrate/20170210022400_create_products.rb
+++ db/migrate/20170210022400_create_products.rb
@@ -1,6 +1,6 @@
-class CreateLists < ActiveRecord::Migration[5.0]
+class CreateProducts < ActiveRecord::Migration[5.0]
   def change
-    create_table :lists do |t|
+    create_table :products do |t|
       t.column :name, :string
       t.column :cost, :float
       t.column :coo, :string
